1ab8398807f6be6d27c8a524d3d9d7fe8ff65ac2
Ruben Karlsson <neburgordon@gmail.com>
2021-07-14 15:30:04 +0200

    generate custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { HighchartsChartModule } from 'highcharts-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -10,6 +12,7 @@ import { FooterComponent } from './footer/footer.component';
 import { FormComponent } from './container/form/form.component';
 import { ServersDisplayComponent } from './container/servers-display/servers-display.component';
 import { ServerComponent } from './container/servers-display/server/server.component';
+import { UnlessDirective } from './directives/unless.directive';
 
 @NgModule({
   declarations: [
@@ -20,8 +23,14 @@ import { ServerComponent } from './container/servers-display/server/server.compo
     FormComponent,
     ServersDisplayComponent,
     ServerComponent,
+    UnlessDirective,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HighchartsChartModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
